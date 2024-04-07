912394fa5d7e3ac4ba28dd346e0f2d9506bd1a9c
SEYEON-PARK <fourleaf123@naver.com>
2024-04-07 18:44:48 +0900

    화면 출력 방법 수정

--- git_test4/src/git_test4/Hello.java
+++ git_test4/src/git_test4/Hello.java
@@ -1,11 +1,11 @@
-package git_test2;
+package git_test4;
 
 public class Hello {
     
     String message = "안녕하세요";
 
-    public void say() {
-        System.out.println(message);
+    public String say() {
+        return message;
     }
 
 }
